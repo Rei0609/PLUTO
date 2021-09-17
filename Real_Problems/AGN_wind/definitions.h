7796a3989b8ad165f3509474d62585cc25cf8754
Rei Tanaka <reipon3012g@gmail.com>
2021-09-17 16:56:33 +0900

    Initial commit
    Already includes stub for AGN wind setup.

---
+++ Real_Problems/AGN_wind/definitions.h
@@ -0,0 +1,30 @@
+#define  PHYSICS                        HD
+#define  DIMENSIONS                     2
+#define  GEOMETRY                       SPHERICAL
+#define  BODY_FORCE                     NO
+#define  COOLING                        NO
+#define  RECONSTRUCTION                 PARABOLIC
+#define  TIME_STEPPING                  RK3
+#define  NTRACER                        1
+#define  PARTICLES                      NO
+#define  USER_DEF_PARAMETERS            3
+
+/* -- physics dependent declarations -- */
+
+#define  DUST_FLUID                     NO
+#define  EOS                            IDEAL
+#define  ENTROPY_SWITCH                 SELECTIVE
+#define  THERMAL_CONDUCTION             NO
+#define  VISCOSITY                      NO
+#define  ROTATING_FRAME                 NO
+
+/* -- user-defined parameters (labels) -- */
+
+#define  POWER                          0
+#define  MACH                           1
+#define  SPEED                          2
+
+/* [Beg] user-defined constants (do not change this line) */
+
+
+/* [End] user-defined constants (do not change this line) */
